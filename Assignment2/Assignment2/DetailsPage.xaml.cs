fd455989258bafe9e956f8e503a3bb39f2e8d97e
Arpan Hazra <arpan.hazra@Macmini2s-Mac-mini.local>
2017-08-02 11:38:18 +0530

    Removed unused namespaces

--- Assignment2/Assignment2/DetailsPage.xaml.cs
+++ Assignment2/Assignment2/DetailsPage.xaml.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using Assignment2.Model;
+﻿using Assignment2.Model;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+
 namespace Assignment2
 {
     public partial class DetailsPage : ContentPage
